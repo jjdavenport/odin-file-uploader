9c255a5569cb6dcca676ffd61ee0f1782e81c425
jjdavenport <jordanjamesdavenport@gmail.com>
2025-08-02 18:49:57 +0100

    add prisma, express session

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import { Header, Wrapper, Container } from "./components/content";
 import { Outlet } from "react-router";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <>
       <Wrapper>
         <Container>
           <Header />
-          <Outlet />
+          <Outlet context={{ loggedIn }} />
         </Container>
       </Wrapper>
     </>
--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const indexRouter = require("./routes/index-router");
 const loginRouter = require("./routes/login-router");
 const registerRouter = require("./routes/register-router");
 const authRouter = require("./routes/auth-router");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.HOSTNAME;
@@ -13,6 +16,21 @@ const HOSTNAME = process.env.HOSTNAME;
 const distPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.json());
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 1000,
+    },
+    resave: true,
+    saveUninitialized: true,
+    secret: "cats",
+    store: new PrismaSessionStore(new PrismaClient(), {
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+      checkPeriod: 2 * 60 * 1000,
+    }),
+  })
+);
 
 app.use("@", (req, res) => {
   res.sendFile(distPath, "index.html");
